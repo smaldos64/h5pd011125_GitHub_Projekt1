5fcd61b809832a7859f0f47034d0838ae356b09f
smaldos64 <gudumholm34@gmail.com>
2025-01-21 10:37:41 +0100

    Endnu en lille ændring for at test GitHub Actions.

--- GitHub_Projekt1/MathOperationsTests.cs
+++ GitHub_Projekt1/MathOperationsTests.cs
@@ -26,7 +26,7 @@ namespace GitHub_Projekt1
             // Arrange
             int a = 2;
             int b = 3;
-            int expected = 9;
+            int expected = 8;
 
             // Act
             int result = MathOperations.Add(a, b);
